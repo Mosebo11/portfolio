5412b76551ceadf3be25b99025c5a1c8cabbff07
Ts'epe Mosebo <85861375+Mosebo11@users.noreply.github.com>
2025-07-08 14:31:58 +0200

    Update Testimonial.js

--- src/components/tesimonial/Testimonial.js
+++ src/components/tesimonial/Testimonial.js
@@ -1,10 +1,9 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 import Slider from "react-slick";
 import { RiStarFill } from "react-icons/ri";
 import { HiArrowRight, HiArrowLeft } from "react-icons/hi";
-import Title from '../layouts/Title'
-import { testimonialOne,testimonialTwo, quote } from "../../assets";
-
+import Title from "../layouts/Title";
+import { testimonialOne, testimonialTwo, quote } from "../../assets";
 
 function SampleNextArrow(props) {
   const { onClick } = props;
@@ -31,62 +30,62 @@ function SamplePrevArrow(props) {
 }
 
 const Testimonial = () => {
-      const [dotActive, setDocActive] = useState(0);
-     const settings = {
-       dots: true,
-       infinite: true,
-       speed: 500,
-       slidesToShow: 1,
-       slidesToScroll: 1,
-       nextArrow:<SampleNextArrow />,
-       prevArrow:<SamplePrevArrow />,
-       beforeChange: (prev, next) => {
-         setDocActive(next);
-       },
-       appendDots: (dots) => (
-         <div
-           style={{
-             borderRadius: "10px",
-             padding: "10px",
-           }}
-         >
-           <ul
-             style={{
-               display: "flex",
-               gap: "15px",
-               justifyContent: "center",
-               marginTop: "20px",
-             }}
-           >
-             {" "}
-             {dots}{" "}
-           </ul>
-         </div>
-       ),
-       customPaging: (i) => (
-         <div
-           style={
-             i === dotActive
-               ? {
-                   width: "12px",
-                   height: "12px",
-                   color: "blue",
-                   background: "#ff014f",
-                   borderRadius: "50%",
-                   cursor: "pointer",
-                 }
-               : {
-                   width: "12px",
-                   height: "12px",
-                   color: "blue",
-                   background: "gray",
-                   borderRadius: "50%",
-                   cursor: "pointer",
-                 }
-           }
-         ></div>
-       ),
-     };
+  const [dotActive, setDocActive] = useState(0);
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+    beforeChange: (prev, next) => {
+      setDocActive(next);
+    },
+    appendDots: (dots) => (
+      <div
+        style={{
+          borderRadius: "10px",
+          padding: "10px",
+        }}
+      >
+        <ul
+          style={{
+            display: "flex",
+            gap: "15px",
+            justifyContent: "center",
+            marginTop: "20px",
+          }}
+        >
+          {" "}
+          {dots}{" "}
+        </ul>
+      </div>
+    ),
+    customPaging: (i) => (
+      <div
+        style={
+          i === dotActive
+            ? {
+                width: "12px",
+                height: "12px",
+                color: "blue",
+                background: "#ff014f",
+                borderRadius: "50%",
+                cursor: "pointer",
+              }
+            : {
+                width: "12px",
+                height: "12px",
+                color: "blue",
+                background: "gray",
+                borderRadius: "50%",
+                cursor: "pointer",
+              }
+        }
+      ></div>
+    ),
+  };
   return (
     <section
       id="testimonial"
@@ -108,11 +107,11 @@ const Testimonial = () => {
                 />
                 <div className="w-full flex flex-col justify-end">
                   <p className="text-xs uppercase text-designColor tracking-wide mb-2">
-                    Bound - Trolola
+                    Mr Takura Bhila
                   </p>
-                  <h3 className="text-2xl font-bold">Jone Duone Joe</h3>
+                  <h3 className="text-2xl font-bold">To-T.Mosebo</h3>
                   <p className="text-base tracking-wide text-gray-500">
-                    Operation Officer
+                    Lecture
                   </p>
                 </div>
               </div>
@@ -122,10 +121,10 @@ const Testimonial = () => {
                   <div className="flex flex-col justify-between lgl:items-center py-6 border-b-2 border-b-gray-900">
                     <div>
                       <h3 className="text-xl lgl:text-2xl font-medium tracking-wide">
-                        Travel Mobile App Design.
+                        Limkokwing University of Creative Tecnology
                       </h3>
                       <p className="text-base text-gray-400 mt-3">
-                        via Upwork - Mar 4, 2015 - Aug 30, 2021 test
+                        via Upwork: Mar - 4 - 2022
                       </p>
                     </div>
                     <div className="text-yellow-500 flex gap-1">
@@ -137,11 +136,11 @@ const Testimonial = () => {
                     </div>
                   </div>
                   <p className="text-base font-titleFont text-gray-400 font-medium tracking-wide leading-6">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. A
-                    dolorum, eos natus ipsum numquam veniam officia
-                    necessitatibus ratione quos debitis exercitationem
-                    repudiandae facilis id neque nihil accusantium perspiciatis
-                    repellat? Iste.
+                    "Tsepe demonstrated excellent coding skills in Python,
+                    JavaScript, PHP, and SQL during his final year AI Chatbot
+                    project. His ability to integrate AI concepts with clean,
+                    functional code was outstanding. Supervising his innovative
+                    and technically sound work was truly a pleasure."
                   </p>
                 </div>
               </div>
@@ -159,11 +158,11 @@ const Testimonial = () => {
                 />
                 <div className="w-full flex flex-col justify-end">
                   <p className="text-xs uppercase text-designColor tracking-wide mb-2">
-                    Bound - Trolola
+                    Johannes Lekheka
                   </p>
-                  <h3 className="text-2xl font-bold">Jone Duone Joe</h3>
+                  <h3 className="text-2xl font-bold">T.Mosebo Employee</h3>
                   <p className="text-base tracking-wide text-gray-500">
-                    Operation Officer
+                    Iconic Footprints Association-IFA
                   </p>
                 </div>
               </div>
@@ -173,11 +172,9 @@ const Testimonial = () => {
                   <div className="flex flex-col justify-between lgl:items-center py-6 border-b-2 border-b-gray-900">
                     <div>
                       <h3 className="text-xl lgl:text-2xl font-medium tracking-wide">
-                        Travel Mobile App Design.
+                        ICT and Data Engineer
                       </h3>
-                      <p className="text-base text-gray-400 mt-3">
-                        via Upwork - Mar 4, 2015 - Aug 30, 2021 test
-                      </p>
+                      <p className="text-base text-gray-400 mt-3">From 2018</p>
                     </div>
                     <div className="text-yellow-500 flex gap-1">
                       <RiStarFill />
@@ -188,7 +185,10 @@ const Testimonial = () => {
                     </div>
                   </div>
                   <p className="text-base font-titleFont text-gray-400 font-medium tracking-wide leading-6">
-                    ts'epe_mosebo.
+                    "Tsepe has shown exceptional skill as an ICT and Data
+                    Engineer during his time at Iconic Footprints Association,
+                    where he effectively applied his expertise in data
+                    management and system development."
                   </p>
                 </div>
               </div>
@@ -206,11 +206,11 @@ const Testimonial = () => {
                 />
                 <div className="w-full flex flex-col justify-end">
                   <p className="text-xs uppercase text-designColor tracking-wide mb-2">
-                    Bound - Trolola
+                    Impact and Holberton Scool
                   </p>
-                  <h3 className="text-2xl font-bold">Jone Duone Joe</h3>
+                  <h3 className="text-2xl font-bold">To T.Mosebo</h3>
                   <p className="text-base tracking-wide text-gray-500">
-                    Operation Officer
+                    Management
                   </p>
                 </div>
               </div>
@@ -220,10 +220,10 @@ const Testimonial = () => {
                   <div className="flex flex-col justify-between lgl:items-center py-6 border-b-2 border-b-gray-900">
                     <div>
                       <h3 className="text-xl lgl:text-2xl font-medium tracking-wide">
-                        Travel Mobile App Design.
+                        Highr Education- ICT
                       </h3>
                       <p className="text-base text-gray-400 mt-3">
-                        via Upwork - Mar 4, 2015 - Aug 30, 2021 test
+                        Since- 2025
                       </p>
                     </div>
                     <div className="text-yellow-500 flex gap-1">
@@ -235,11 +235,11 @@ const Testimonial = () => {
                     </div>
                   </div>
                   <p className="text-base font-titleFont text-gray-400 font-medium tracking-wide leading-6">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. A
-                    dolorum, eos natus ipsum numquam veniam officia
-                    necessitatibus ratione quos debitis exercitationem
-                    repudiandae facilis id neque nihil accusantium perspiciatis
-                    repellat? Iste.
+                    "Tsepe is a committed and knowledgeable ICT educator who
+                    consistently delivers engaging and well-prepared lessons.
+                    His expertise in programming, data systems, and real-world
+                    tech applications greatly benefits our students. He is a
+                    valued member of our academic team and a strong role model."
                   </p>
                 </div>
               </div>
@@ -249,6 +249,6 @@ const Testimonial = () => {
       </div>
     </section>
   );
-}
+};
 
-export default Testimonial
+export default Testimonial;
