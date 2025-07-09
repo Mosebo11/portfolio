7b0c14efcdc4de67fa8492fff86c0af9a4c3606c
Ts'epe Mosebo <85861375+Mosebo11@users.noreply.github.com>
2025-07-09 13:39:15 +0200

    Update Media.jsx

--- src/components/banner/Media.jsx
+++ src/components/banner/Media.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaFacebookF, FaWatsapp, FaLinkedinIn, FaReact } from "react-icons/fa";
+import { FaFacebookF, FaWhatsapp, FaLinkedinIn, FaReact } from "react-icons/fa";
 import { SiTailwindcss, SiFigma, SiNextdotjs } from "react-icons/si";
 const Media = () => {
   return (
