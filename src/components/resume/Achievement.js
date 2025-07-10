8778e52c0a48002aef6fd8241ddb93c0b4e6a704
Ts'epe Mosebo <85861375+Mosebo11@users.noreply.github.com>
2025-07-10 10:45:44 +0200

    Update Achievement.js
    add achievements

--- src/components/resume/Achievement.js
+++ src/components/resume/Achievement.js
@@ -9,46 +9,19 @@ const Achievement = () => {
       animate={{ opacity: 1, transition: { duration: 0.5 } }}
       className="py-12 font-titleFont flex gap-20"
     >
-      {/* Company Experience */}
-      <div>
-        <div className="py-12 font-titleFont flex flex-col gap-4">
-          <p className="text-sm text-designColor tracking-[4px]">2018 - 2024</p>
-          <h2 className="text-4xl font-bold">Company Experience</h2>
-        </div>
-        <div className="mt-14 w-full h-auto border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
-          <ResumeCard
-            title="Data Collector"
-            subTitle="Medical Care and Development International (MCDI), 2018 - 2019"
-            result="Report Completed"
-            des="Collected data from patients at Ts’akholo Health Centre, analyzed results using Excel, and submitted reports for medical service improvement."
-          />
-          <ResumeCard
-            title="Peer Educator & Data Collector"
-            subTitle="LENASO (Lesotho Network of AIDs Service Organization), 2023 - 2024"
-            result="Community Impact"
-            des="Educated communities about HIV/AIDS, recorded participant data, and submitted reports to Mafeteng Hospital."
-          />
-          <ResumeCard
-            title="System Administrator & Data Engineer"
-            subTitle="Iconic FootPrints Association, 2023 - 2024"
-            result="Secured System"
-            des="Managed networks and analyzed data to generate actionable reports and insights for the organization."
-          />
-        </div>
-      </div>
-
-      {/* Job Experience */}
+      
+      {/* Achievements */}
       <div>
         <div className="py-12 font-titleFont flex flex-col gap-4">
           <p className="text-sm text-designColor tracking-[4px]">2023</p>
-          <h2 className="text-4xl font-bold">Job Experience</h2>
+          <h2 className="text-4xl font-bold">Achievements</h2>
         </div>
         <div className="mt-14 w-full h-auto border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
           <ResumeCard
-            title="Invigilator"
-            subTitle="Motsekuoa High School, 2023"
-            result="Successfully Managed Exams"
-            des="Oversaw examination sessions, enforced ECOL rules, handled student registration, and submitted daily reports to school administration."
+            title="Founder"
+            subTitle="__init__ technologies, 2024"
+            result="Successfully Information Technology Company"
+            des="__init__ Technologies is a brilliant reflection of innovation from the ground up—its name alone speaks to starting something powerful and foundational. A bold step into tech, driven by vision and code."
           />
           <ResumeCard
             title="Software Engineering Graduate"
