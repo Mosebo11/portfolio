54e9c7618859edf9d7cb46033ac8f482b09d8b64
Ts'epe Mosebo <85861375+Mosebo11@users.noreply.github.com>
2025-07-10 11:38:14 +0200

    Update Media.jsx
    fixing fb link

--- src/components/banner/Media.jsx
+++ src/components/banner/Media.jsx
@@ -1,23 +1,28 @@
-import React from 'react'
+import React from "react";
 import { FaFacebookF, FaWhatsapp, FaLinkedinIn, FaReact } from "react-icons/fa";
-import { SiTailwindcss, SiFigma, SiNextdotjs } from "react-icons/si";
+import {
+  SiTailwindcss,
+  SiFigma,
+  SiNextdotjs,
+  SiPython,
+  SiTensorflow,
+  SiTableau,
+} from "react-icons/si";
+
 const Media = () => {
   return (
     <div className="flex flex-col xl:flex-row gap-6 lgl:gap-0 justify-between">
-        <div>
-          <h2 className="text-base uppercase font-titleFont mb-4">
-            Find me in
-          </h2>
-          <div className="flex gap-4">
+      <div>
+        <h2 className="text-base uppercase font-titleFont mb-4">Find me in</h2>
+        <div className="flex gap-4">
           <a
-            href="https://www.facebook.com/ke.lokisa.lifon"
+            href="https://www.facebook.com/ke.lokisa.lifono?mibextid=ZbWKwL"
             target="_blank"
             rel="noopener noreferrer"
             className="bannerIcon"
           >
             <FaFacebookF />
           </a>
-
           <a
             href="https://wa.me/26659981177"
             target="_blank"
@@ -26,7 +31,6 @@ const Media = () => {
           >
             <FaWhatsapp />
           </a>
-
           <a
             href="https://www.linkedin.com/in/tsepe-mosebo"
             target="_blank"
@@ -36,28 +40,38 @@ const Media = () => {
             <FaLinkedinIn />
           </a>
         </div>
-        </div>
-        <div>
-          <h2 className="text-base uppercase font-titleFont mb-4">
-            BEST SKILL ON
-          </h2>
-          <div className="flex gap-4">
-            <span className="bannerIcon">
-              <FaReact />
-            </span>
-            <span className="bannerIcon">
-              <SiNextdotjs />
-            </span>
-            <span className="bannerIcon">
-              <SiTailwindcss />
-            </span>
-            <span className="bannerIcon">
-              <SiFigma />
-            </span>
-          </div>
+      </div>
+
+      <div>
+        <h2 className="text-base uppercase font-titleFont mb-4">
+          BEST SKILL ON
+        </h2>
+        <div className="flex gap-4 flex-wrap">
+          <span className="bannerIcon">
+            <FaReact />
+          </span>
+          <span className="bannerIcon">
+            <SiNextdotjs />
+          </span>
+          <span className="bannerIcon">
+            <SiTailwindcss />
+          </span>
+          <span className="bannerIcon">
+            <SiFigma />
+          </span>
+          <span className="bannerIcon">
+            <SiPython />
+          </span>
+          <span className="bannerIcon">
+            <SiTensorflow />
+          </span>
+          <span className="bannerIcon">
+            <SiTableau />
+          </span>
         </div>
       </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Media
+export default Media;
